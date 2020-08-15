bf6a9e1443e556920cdbcaf97cff8d782f0c2e91
dvcorreia <dv_correia@hotmail.com>
2020-08-15 20:50:19 +0100

    project directory organization + experience, project and education sections

---
+++ clear.sh
@@ -0,0 +1,2 @@
+rm -f *.bcf *.xml *.gz __*.tex *.aux *.bbl *.fdb_latexmk *.fls *.log *.out *.blg *.xmpi
+rm -rf */**/*.bcf */**/*.xml */**/*.gz */**/__*.tex */**/*.aux */**/*.bbl */**/*.fdb_latexmk */**/*.fls */**/*.log */**/*.out */**/*.blg */**/*.xmpi */**/*.pdf
